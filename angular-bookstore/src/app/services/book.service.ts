09ceaccb35043793d08ca78dee5d789b0d65c150
anmol <anmol@MSI>
2020-11-21 21:33:53 +0530

    Created the Angular Application and Completed the feature One.

---
+++ angular-bookstore/src/app/services/book.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Book } from '../common/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private baseUrl = "http://localhost:8080/api/v1/books";
+  constructor(private httpClient: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.httpClient.get<GetResponseBooks>(this.baseUrl).pipe(
+      map(response => response.body)
+    );
+  }
+}
+
+interface GetResponseBooks {
+  body: Book[];
+}
--- bookstore/src/main/java/com/anmolahuja/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/anmolahuja/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.anmolahuja.bookstore.service.BookService;
 
 @Configuration
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class BookController {
 
 	@Autowired
